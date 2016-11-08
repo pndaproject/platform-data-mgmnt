2dd3cbd39a6d75818cc962be7f355335ef4ae933
trsmith2 <trsmith2@cisco.com>
2016-11-08 19:03:18 +0000

    Introduce pylint checking in build
    PNDA-2180

--- build.sh
+++ build.sh
@@ -10,12 +10,37 @@ function error {
     exit -1
 }
 
+function code_quality_error {
+    echo "${1}"
+}
+
 echo -n "Apache Maven 3.0.5: "
 if [[ $(mvn -version 2>&1) == *"Apache Maven 3.0.5"* ]]; then
     echo "OK"
 else
     error
 fi
+
+function check_pylint {
+    PYLINTOUT=$(find . -type f -name '*.py' | grep -vi __init__ | xargs pylint)
+    SCORE=$(echo ${PYLINTOUT} | grep -Po '(?<=rated at ).*?(?=/10)')
+    echo ${SCORE}
+    if [[ $(bc <<< "${SCORE} > 9") == 0 ]]; then
+        code_quality_error "${PYLINTOUT}"
+    fi
+}
+
+BASE=${PWD}
+
+echo -n "Code quality (data-service) : "
+cd ${BASE}/data-service/src/main/resources
+check_pylint
+
+echo -n "Code quality (hdfs-cleaner) : "
+cd ${BASE}/hdfs-cleaner/src/main/resources
+check_pylint
+
+cd ${BASE}
 
 mkdir -p pnda-build
 cd data-service
