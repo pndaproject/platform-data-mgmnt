81b153bca0a47de1c28630e0e1b532b0c5e733bb
sushant <sushant.pandey@maplelabs.com>
2017-12-30 13:33:13 +0000

    PNDA-3127: Post ingress aggregation for Kafka datasets

--- hdfs-cleaner/src/main/resources/hdfs-cleaner.py
+++ hdfs-cleaner/src/main/resources/hdfs-cleaner.py
@@ -371,6 +371,19 @@ def main():
                           general_dirs_to_clean, NEG_SIZE)
     jobs.append(job_common_dirs)
 
+    # staging dataset to clean
+    staging_dataset_to_clean = properties['staging_dataset_to_clean']
+    if 'mode' in staging_dataset_to_clean.keys():
+	if staging_dataset_to_clean['mode'] == 'delete':
+	   cmd = delete_cmd
+	else:
+	   cmd = archive_cmd
+        age = staging_dataset_to_clean.get('age', 1)
+        staging_dataset_location = staging_dataset_to_clean['staging_dataset_location']
+        job_staging_dirs = JOB('clean_staging_dataset', hdfs, cleanup_on_age, cmd,
+                              staging_dataset_location, age)
+        jobs.append(job_staging_dirs)
+
     old_dirs_to_clean = properties['old_dirs_to_clean']
     for entry in old_dirs_to_clean:
         print entry['name']
