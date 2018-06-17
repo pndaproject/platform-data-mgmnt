b61a69d3c9ac4eae2cc6774a9584170a2f8e6032
trsmith2 <trsmith2@cisco.com>
2018-06-17 12:10:53 +0100

    Revert "PNDA-4426: Dataset service is missing a log level config setting"

--- data-service/src/main/resources/config.py
+++ data-service/src/main/resources/config.py
@@ -42,5 +42,3 @@ def define_options():
     define("cm_host", default='localhost', help="The cluster manager interface", type=str)
     define("cm_user", default='admin', help="The user name for cluster manager", type=str)
     define("cm_pass", default='admin', help="The password for cluster manager", type=str)
-    define("log_level", default='INFO', help="The log level setting for logging", type=str)
-    define("log_file_prefix", default='dataservice.log', help="The log file for logging", type=str)
